b7fe0005861091359a6c972dd0548e3f067f7587
CaptainPAG <line.6th818@gmail.com>
2024-03-12 16:14:59 +0900

    modify:ページきりかえ処理をflutter_hooksを利用するように変更

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:codelab/pages/favorites_page.dart';
 import 'package:codelab/pages/generator_page.dart';
 import 'package:codelab/states/my_app_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -27,18 +28,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
+class MyHomePage extends HookWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final selectedIndex = useState(0);
     Widget page;
-    switch (selectedIndex) {
+    switch (selectedIndex.value) {
       case 0:
         page = GeneratorPage();
         break;
@@ -67,11 +64,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       label: Text('Favorites'),
                     ),
                   ],
-                  selectedIndex: selectedIndex,
+                  selectedIndex: selectedIndex.value,
                   onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
+                    selectedIndex.value = value;
                   },
                 ),
               ),
